0745e1f7785987004aff1a1894029b3c356e2b47
Leboutaleb <almostaphasmart@MacBook-Pro-2.local>
2025-05-26 20:34:10 +0100

    Remove disabled folders and create Footer.css

--- client/src/app/savoir-faire-disabled/page.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import SavoirFaire from '../../components/savoir-faire/SavoirFaire';
-
-export default function SavoirFairePage() {
-  return <SavoirFaire />;
-}
