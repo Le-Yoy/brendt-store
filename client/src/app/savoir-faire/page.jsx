5821d3fa8507b9ed33a0fc62df67f972dba8251a
Leboutaleb <almostaphasmart@MacBook-Pro-2.local>
2025-05-26 19:13:06 +0100

    Remove .jsx extension from import

--- client/src/app/savoir-faire/page.jsx
+++ client/src/app/savoir-faire/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import SavoirFaire from '../../components/savoir-faire/SavoirFaire.jsx';
+import SavoirFaire from '../../components/savoir-faire/SavoirFaire';
 
 export default function SavoirFairePage() {
   return <SavoirFaire />;
